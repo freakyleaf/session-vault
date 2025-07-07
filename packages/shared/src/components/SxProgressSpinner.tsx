0f5aeefd6036038062a69c7fa9023de233d671a9
freakyleaf <garethjames@outlook.com>
2025-07-07 11:36:24 +0100

    feat: added progess spinner

---
+++ packages/shared/src/components/SxProgressSpinner.tsx
@@ -0,0 +1,11 @@
+import { ProgressSpinner } from 'primereact/progressspinner';
+
+function SxProgressSpinner() {
+  return (
+    <div className="flex justify-content-center">
+      <ProgressSpinner />
+    </div>
+  );
+}
+
+export default SxProgressSpinner;
