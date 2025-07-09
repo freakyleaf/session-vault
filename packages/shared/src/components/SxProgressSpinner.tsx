bc550da2e30b6542d988c0bdc64b76a75edb9053
freakyleaf <garethjames@outlook.com>
2025-07-09 12:05:37 +0100

    chore: added class to loading spinner component

--- packages/shared/src/components/SxProgressSpinner.tsx
+++ packages/shared/src/components/SxProgressSpinner.tsx
@@ -2,7 +2,7 @@ import { ProgressSpinner } from 'primereact/progressspinner';
 
 function SxProgressSpinner() {
   return (
-    <div className="flex justify-content-center">
+    <div className="flex justify-content-center my-5">
       <ProgressSpinner />
     </div>
   );
